438c3b89118ccc04a9a546f213d80a47b247ecd4
Ali Hassan <alihassanuog50@gmail.com>
2024-05-30 02:51:38 +0500

    completed app

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-const electron = require("electron");
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const path = require("path");
 
 let mainWindow;
 let addWindow;
+
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
+  });
   mainWindow.loadURL("file://" + __dirname + "/main.html");
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
+
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
 function createAddWindow() {
   addWindow = new BrowserWindow({
-    width: 300,
-    height: 200,
+    width: 400,
+    height: 300,
     title: "Add New Todo",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
   });
   addWindow.loadURL("file://" + __dirname + "/add.html");
-  //   const mainMenu = Menu.buildFromTemplate(menuTemplate);
-  //   Menu.setApplicationMenu(mainMenu);
+  addWindow.on("closed", () => {
+    addWindow = null;
+  });
 }
+
+ipcMain.on("todo:add", (event, todo) => {
+  mainWindow.webContents.send("todo:add", todo);
+  addWindow.close();
+});
 
 const menuTemplate = [
   {
@@ -29,6 +53,12 @@ const menuTemplate = [
         label: "New Todo",
         click() {
           createAddWindow();
+        },
+      },
+      {
+        label: "Clear Todos",
+        click() {
+          mainWindow.webContents.send("todo:clear");
         },
       },
       {
@@ -42,7 +72,25 @@ const menuTemplate = [
   },
 ];
 
-// Add Mac specific menu to solve issue with menu not appearing in MAC OS
 if (process.platform === "darwin") {
   menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "Developer",
+    submenu: [
+      {
+        role: "reload",
+      },
+      {
+        label: "Toggle DevTools",
+        accelerator:
+          process.platform === "darwin" ? "Command+Alt+I" : "Ctrl+Shift+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  });
 }
