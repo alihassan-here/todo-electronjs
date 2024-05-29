612885f404407531feddb15c7d8a9397d058c3b3
Ali Hassan <alihassanuog50@gmail.com>
2024-05-29 11:45:33 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const electron = require("electron");
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+let addWindow;
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL("file://" + __dirname + "/main.html");
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add New Todo",
+  });
+  addWindow.loadURL("file://" + __dirname + "/add.html");
+  //   const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  //   Menu.setApplicationMenu(mainMenu);
+}
+
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "New Todo",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+// Add Mac specific menu to solve issue with menu not appearing in MAC OS
+if (process.platform === "darwin") {
+  menuTemplate.unshift({});
+}
